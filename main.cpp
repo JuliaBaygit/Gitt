1b94266b483c2bc363f346d3804a1fa9e8c77606
JuliaBaygit <45544427+JuliaBaygit@users.noreply.github.com>
2018-12-03 02:50:10 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "std.h"
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a;
+	int b;
+	cout << "Введите a: " << endl;
+	cin >> a;
+	cout << "Введите b: " << endl;
+	cin >> b;
+	cout << "Сумма" << endl;
+	cout << a + b << endl;
+	_getch();
+	return 0;
+}
